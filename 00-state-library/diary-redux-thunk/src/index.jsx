d9bd963bf7b7f1a637d6393e83f4f61483aa5fce
Superduper-India <sunyoung1542@gmail.com>
2022-12-16 17:13:04 +0900

    [feat] 사용자는 메인에서 글을 볼 수 있다 react query를 대체하여 redux thunk를 사용했습니다. 서버데이터를 가져와서 status와 함께 스토어에 값을 저장해줍니다.

--- 00-state-library/diary-redux-thunk/src/index.jsx
+++ 00-state-library/diary-redux-thunk/src/index.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {
-  QueryClient,
-  QueryClientProvider,
-} from '@tanstack/react-query';
-
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from "react-redux";
 
+import store from "./store";
 import App from './App';
 
 // 클라이언트를 새롭게 생성
@@ -16,10 +14,13 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   // 클라이언트를 앱에 제공
   <QueryClientProvider client={queryClient}>
-    <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </React.StrictMode>
+    {/*Provider를 앱에 제공*/}
+    <Provider store={store}>
+      <React.StrictMode>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </React.StrictMode>
+    </Provider>
   </QueryClientProvider>
 );
