ed72beb90a25ffb381bb1003dd7f465651c48979
Superduper-India <sunyoung1542@gmail.com>
2022-12-16 23:49:34 +0900

    FashionCoord-e 앱의 탑네비게이션바와 홈페이지에 디자인을 적용했습니다 - 2

--- 00-state-library/diary-redux-thunk/src/components/List.jsx
+++ 00-state-library/diary-redux-thunk/src/components/List.jsx
@@ -1,15 +1,17 @@
+import React from 'react';
+
 import { Link } from 'react-router-dom';
 
 // posts 배열을 받는다.
 export default function List({ posts }) {
   return (
     <>
-      <ul className='tasks-box'>
+      <ul>
         {posts.map(post => (
           <li key={post.id} >
             <h2>{post.title}</h2>
             <Link to={`/detail/${post.id}`}>
-              <p>See More</p>
+              See More
             </Link>
           </li>
         ))}
