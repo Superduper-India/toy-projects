aee5337e4aad7a1e02a66b4bda6abcecc763f961
Superduper-India <sunyoung1542@gmail.com>
2022-12-18 00:20:40 +0900

    FashionCoord-e 앱의 홈페이지와 작성페이지에  디자인을 적용하라

--- 00-state-library/diary-redux-thunk/src/components/List.jsx
+++ 00-state-library/diary-redux-thunk/src/components/List.jsx
@@ -1,21 +1,83 @@
-import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useLayoutEffect, useRef, useState } from 'react';
 
-import { Link } from 'react-router-dom';
+import {
+  CarouselWrapper,
+  CarouselButtons,
+  CarouselLeftButton,
+  CarouselRightButton,
+} from '../styles/Styles';
 
-// posts 배열을 받아서 화면에 그려줍니다.
+// posts 배열을 받는다.
 export default function List({ posts }) {
+  const navigate = useNavigate();
+
+  const listRef = useRef();
+
+  const [currIndex, setCurrIndex] = useState(0);
+  const [transX, setTransX] = useState(0);
+
+  // 처음 x좌표를 설정하는 로직
+  useLayoutEffect(() => {
+    const getCoordinate = () => {
+      // 직접적으로 dome조작하는 함수
+      const listLeft = listRef.current.getBoundingClientRect().left;
+      setTransX(listLeft);
+    };
+    getCoordinate();
+  }, []);
+
+  // 데이터가 많이있으면 버튼클릭할때 dispatch가 걸리게 해서 다음페이지까지만 받아오기...
+  const handleClickNavBtn = (direction) => {
+    if (direction === 'right') {
+      const listNode_currentX = listRef.current.childNodes[currIndex + 1].getBoundingClientRect().left;
+      listRef.current.style.transform =
+        `translateX(-${listNode_currentX - transX}px)`;
+      setCurrIndex(currIndex + 2);
+    } else {
+      const listNode_currentX = listRef.current.childNodes[currIndex - 1].getBoundingClientRect().left;
+      listRef.current.style.transform =
+        `translateX(-${listNode_currentX - transX}px)`;
+      setCurrIndex(currIndex - 2);
+    }
+  };
+
   return (
     <>
-      <ul>
-        {posts.map(post => (
-          <li key={post.id} >
-            <h2>{post.title}</h2>
-            <Link to={`/detail/${post.id}`}>
-              See More
-            </Link>
-          </li>
-        ))}
-      </ul>
+      <CarouselButtons>
+        <CarouselLeftButton
+          type="button"
+          onClick={() => handleClickNavBtn('left')}
+          className="left-btn"
+        >
+          {'<'}
+        </CarouselLeftButton>
+        <CarouselRightButton
+          type="button"
+          onClick={() => handleClickNavBtn('right')}
+          className="right-btn"
+        >
+          {'>'}
+        </CarouselRightButton>
+      </CarouselButtons>
+      <CarouselWrapper>
+        <ul className="carousel" ref={listRef}>
+          {posts.map(post => (
+            <li key={post.id}>
+              <div>
+                <p>{post.title}</p>
+                <button
+                  type="button"
+                  className="detail-btn"
+                  onClick={() => { navigate(`/detailPage/${post.id}`, { state: post }); }}
+                >
+                  {' 더보기 >'}
+                </button>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </CarouselWrapper>
     </>
   );
 }
