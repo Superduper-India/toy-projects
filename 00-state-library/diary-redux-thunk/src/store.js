958ef31276c695fde7f5bcc173f40cc0e78d9af0
Superduper-India <sunyoung1542@gmail.com>
2022-12-21 13:38:50 +0900

    FashionCoord-e 앱의 action에 직렬화가 불가능한 값을 전달했다는 에러를 해결하라 store에 middleware 프로퍼티를 추가해서 serializableCheck를 false로 설정했습니다.

--- 00-state-library/diary-redux-thunk/src/store.js
+++ 00-state-library/diary-redux-thunk/src/store.js
@@ -1,9 +1,12 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
 
-import postReducer from "./slice";
+import postReducer from './slice';
 
 const store = configureStore({
   reducer: { postReducer },
+  middleware: getDefaultMiddleware({
+    serializableCheck: false,
+  }),
 });
 
 export default store;
