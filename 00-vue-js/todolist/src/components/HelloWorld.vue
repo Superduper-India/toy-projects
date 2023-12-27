24a81ac88031c8050c5475eff6d95389ee78ccb4
Sunyoung <sunyoung1542@gmail.com>
2023-12-27 18:30:09 +0900

    Vue 템플릿 구문에 대해 공부한 내용 정리

--- 00-vue-js/todolist/src/components/HelloWorld.vue
+++ 00-vue-js/todolist/src/components/HelloWorld.vue
@@ -2,22 +2,39 @@
   <div class="hello">
     <!--템플릿 구문: 속성 예시-->
     <button v-bind:disabled="isButtonDisabled">Button</button>
+    <br/>
     <!--템플릿 구문: v-once-->
     <!--html코드로 출력된 이후에 어떤 후처리가 있더라도 처음에 출력한 값을 유지시킬 때 사용-->
     <h1 v-once>This will never change: {{ title }}</h1>
     <p>{{ sayHello() }}</p>
+    <br/>
     <!--템플릿 구문: mustaches-->
     <!--html속성 안에서는 사용할 수 없다-->
     <h1>{{ msg }}</h1>
+    <br/>
     <!--템플릿 구문: 인수-->
     <!--지시어의 콜론 뒤에 작성하는 값으로 인수를 전달할 수 있다 아래는 그 예시-->
     <a v-bind:href="url">Attribute Arguments</a>
     <a v-on:click="doSomething">Event Arguments</a>
+    <!--아래는 축약버전-->
+    <a :click="doSomething">Event Arguments</a>
+    <br/>
     <!--템플릿 구문: 동적 인수-->
     <!--지시어의 콜론 뒤에 작성하는 값으로 인수를 전달할 수 있다 아래는 그 예시-->
+    <!--string 아니면 null값만 올 수 있고 대문자, 공백이나 따옴표 없이 사용해야한다-->
     <a v-bind:[attributeName]="url">Attribute Dynamic Arguments</a>
     <a v-on:[eventName]="doSomething">Event Dynamic Arguments</a>
-    <p>
+    <!--아래는 축약버전-->
+    <a :[attributeName]="url">Attribute Dynamic Arguments</a>
+    <a @[eventName]="doSomething">Event Dynamic Arguments</a>
+    <br/>
+    <!--템플릿 구문: Modifiers-->
+    <!--아래와 같이 .prevent수정자는 v-on지시어가 트리거된 이벤트에서-->
+    <!--event.preventDefault()를 호출하도록 지시한다-->
+    <form v-on:submit.prevent="onSubmit">Prevent Submit!!!</form>
+    <br/>
+  
+    <!-- <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
@@ -42,7 +59,7 @@
       <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
       <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
       <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    </ul> -->
   </div>
 </template>
 
