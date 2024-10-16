7e51e115552da51dafe77b425dcb98d6443fe7c8
Superduper-India <sunyoung1542@gmail.com>
2024-10-16 16:37:45 +0900

    Vue 프로젝트 - 어댑터 패턴 활용 예제

--- 00-vue-js/ss/src/App.vue
+++ 00-vue-js/ss/src/App.vue
@@ -1,59 +1,84 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
-    <DynamicScroller :minItemSize="50" class="scroller" :items="items">
+    <ul>
+      <li
+        v-for="(item, index) in weekNames"
+        :key="index + 'weeks'"
+        :class="item.isSelected ? 'active' : ''"
+        @click="handleClickWeek(index)">
+        {{ item.name }}
+      </li>
+    </ul>
+    <!-- <DynamicScroller :minItemSize="50" class="scroller" :items="items">
       <template #default="{ item, index }">
         <div class="item">{{ index }} - {{ item.name }}</div>
       </template>
-    </DynamicScroller>
-    <button class="btn" @click="handleClick" v-bind="buttonProps">Click me</button>
+    </DynamicScroller> -->
+    <!-- <button class="btn" @click="handleClick" v-bind="buttonProps">Click me</button> -->
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-import { DynamicScroller } from 'vue-virtual-scroller';
-import { useButton } from '@headlessui/vue';
-import { defineComponent } from '@vue/composition-api';
+// import HelloWorld from './components/HelloWorld.vue';
+// import { DynamicScroller } from 'vue-virtual-scroller';
+// import { useButton } from '@headlessui/vue';
+// import { defineComponent } from '@vue/composition-api';
 
-defineComponent({
-  setup() {
-    // useButton으로 버튼에 필요한 속성 가져오기
-    const buttonProps = useButton();
+// defineComponent({
+//   setup() {
+//     // useButton으로 버튼에 필요한 속성 가져오기
+//     const buttonProps = useButton();
 
-    const handleClick = () => {
-      console.log('Button clicked!');
-    };
+//     const handleClick = () => {
+//       console.log('Button clicked!');
+//     };
 
-    return { buttonProps, handleClick };
-  },
-});
+//     return { buttonProps, handleClick };
+//   },
+// });
 
 class ChartAdapter {
   constructor(receivedApiData) {
-    this.aa = receivedApiData.first_name;
-    this.bb = receivedApiData.last_name;
-    this.cc = receivedApiData.age;
+    this.modifiedData = receivedApiData;
   }
 
-  getFullName() {
-    return `${this.aa} ${this.bb}`;
+  getWeekName(targetIdx = 0) {
+    const weeks = ['월', '화', '수', '목', '금', '토', '일'];
+    return this.modifiedData.map((item, idx) => {
+      return {
+        name: weeks[idx],
+        value: item.avg_score,
+        y: item.avg_score,
+        isSelected: targetIdx === idx,
+      };
+    });
+  }
+  getTimeName(targetIdx = 0) {
+    const times = ['6시 이전', '6시~11시', '11시~16시', '16시 이후'];
+    return this.modifiedData.map((item, idx) => {
+      return {
+        name: times[idx],
+        value: item.avg_score,
+        y: item.avg_score,
+        isSelected: targetIdx === idx,
+      };
+    });
   }
 }
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
-    DynamicScroller,
+    // DynamicScroller,
   },
   created() {
     this.fetchChartData();
   },
   data() {
     return {
-      chartData: {},
+      weekNames: [], // 요일별 가공된 데이터
+      weekInstance: {}, // 요일별 인스턴스
       items: Array.from({ length: 1000 }).map((_, index) => ({
         name: `Item ${index}`,
         id: index,
@@ -61,15 +86,30 @@ export default {
     };
   },
   methods: {
+    handleClickWeek(targetIdx) {
+      this.weekNames = this.weekInstance.getWeekName(targetIdx);
+    },
     fetchChartData() {
-      const apiData = {
-        first_name: 'John',
-        last_name: 'Doe',
-        age: 30,
-      };
-      this.chartData = new ChartAdapter(apiData);
-      console.log('ddddd :: ', this.chartData);
-      console.log('ddddd :: ', this.chartData.getFullName());
+      const weekData = [
+        {
+          week: 'mon',
+          avg_score: 44.12,
+        },
+        {
+          week: 'thu',
+          avg_score: 44.12,
+        },
+        {
+          week: 'wed',
+          avg_score: 44.12,
+        },
+        {
+          week: 'thr',
+          avg_score: 44.12,
+        },
+      ];
+      this.weekInstance = new ChartAdapter(weekData);
+      this.weekNames = this.weekInstance.getWeekName();
     },
   },
 };
@@ -105,5 +145,10 @@ export default {
 
 .btn:hover {
   background-color: #2980b9;
+}
+
+.active {
+  background-color: #0079ff;
+  color: #ffffff;
 }
 </style>
