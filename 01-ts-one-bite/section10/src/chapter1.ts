17c886e738ade7a447486f90f88203d52dd4e423
Superduper-India <sunyoung1542@gmail.com>
2024-12-06 14:56:37 +0900

    10장 조건부 타입 - Record, Pick, Omit

--- 01-ts-one-bite/section10/src/chapter1.ts
+++ 01-ts-one-bite/section10/src/chapter1.ts
@@ -1,8 +1,6 @@
 // 맵드 타입 기반의 유틸리티 타입
 // Partial, Required, Readonly
 
-// 1. Partial<T>
-// 특정 객체 타입의 모든 프로퍼티를 선택적 프로퍼티로 바꿔주는 타입
 interface Post {
   title: string;
   tags: string[];
@@ -10,19 +8,18 @@ interface Post {
   thumbnailURL?: string;
 }
 
-// 맵드 타입을 사용하여 직접 구현해보기
+// 1. Partial<T>
+// 특정 객체 타입의 모든 프로퍼티를 선택적 프로퍼티로 바꿔주는 타입
+// => 맵드 타입을 사용하여 구현
 type Partial<T> = {
   [key in keyof T]?: T[key];
 };
 
 const draft: Partial<Post> = {
   // ❌ tags 프로퍼티가 없음
-  title: '제목은 나중에 짓자...',
-  content: '초안...',
+  title: "제목은 나중에 짓자...",
+  content: "초안...",
 };
-
-// 2. Required<T>
-// 특정 객체 타입의 모든 프로퍼티를 필수 프로퍼티로 바꿔주는 타입
 
 interface Post {
   title: string;
@@ -31,16 +28,19 @@ interface Post {
   thumbnailURL?: string;
 }
 
+// 2. Required<T>
+// 특정 객체 타입의 모든 프로퍼티를 필수 프로퍼티로 바꿔주는 타입
+// => 맵드 타입을 사용하여 구현
 type Required<T> = {
   [key in keyof T]-?: T[key];
 };
 
 const withThumbnailPost: Required<Post> = {
   // ❌
-  title: '한입 타스 후기',
-  tags: ['ts'],
-  content: '',
-  thumbnailURL: 'https://...',
+  title: "한입 타스 후기",
+  tags: ["ts"],
+  content: "",
+  thumbnailURL: "https://...",
 };
 
 // 3. Readonly<T>
@@ -57,9 +57,9 @@ type Readonly<T> = {
 };
 
 const readonlyPost: Readonly<Post> = {
-  title: '보호된 게시글입니다.',
+  title: "보호된 게시글입니다.",
   tags: [],
-  content: '',
+  content: "",
 };
 
 // readonlyPost.content = '해킹당함';
---
+++ 01-ts-one-bite/section10/src/chapter2.ts
@@ -0,0 +1,68 @@
+// 맵드 타입 기반의 유틸리티 타입2
+// Record, Pick, Omit
+
+// 1. Pick타입
+// => 객체 타입으로부터 특정 프로퍼티만 골라내는 타입
+interface Post {
+  title: string;
+  tags: string[];
+  content: string;
+  thumbnailURL?: string;
+}
+
+type Pick<T, K extends keyof T> = {
+  // K extends 'title' | 'tags' | 'content' | 'thumnailURL'
+  // 즉, 좌항이 우항의 슈퍼타입, 서브타입이 아니면 성립하지 않는다.
+  [key in K]: T[key];
+};
+
+// Post타입으로부터 'title'과 'content'타입만 가져온 새로운 타이핑을 한다.
+const legacyPost: Pick<Post, "title" | "content"> = {
+  title: "dd",
+  content: "dddd",
+};
+
+// 2. Omit타입
+// => 객체 타입으로부터 특정 프로퍼티를 제거하는 타입
+// 앞서본 예제처럼 Pick타입에서 특정 프로퍼티를 일일이 추가하는 작업은 프로퍼티가 많아지면 어렵다.
+
+type Omit<T, K extends keyof T> = Pick<T, Exclude<keyof T, K>>;
+// 우항을 하나씩 풀어보면,
+// T = Post, K = 'title'
+// Pick<Post, Exclude<keyof Post, 'title'>>
+// Pick<Post, Exclude<'title' | 'tags' | 'content' | 'thumbnailURL', 'title'>>
+// Exclude는 T로부터 K를 제거하므로,
+// Pick<Post, 'content' | 'tags' | 'thumbnailURL'>가 된다.
+
+const noTitlePost: Omit<Post, "title"> = {
+  // ❌
+  content: "",
+  tags: [],
+  thumbnailURL: "",
+};
+
+// 3. Record타입
+// => 객체 타입을 만들어주는 유틸리티 타입
+// 첫번째 타입 변수로 객체타입의 키, 두번째 타입 변수로 객체타입의 값을 받는다.
+
+// 화면 크기에 따라 3가지 버전의 썸네일을 지원하고, 아래는 버전에 따른 타입이다.
+// 하지만 버전이 늘어날 수록 중복코드를 추가하는 문제가 발생한다.
+type ThumbnailLegacy = {
+  large: {
+    url: string;
+  };
+  medium: {
+    url: string;
+  };
+  small: {
+    url: string;
+  };
+};
+
+// 어떤 객체 타입일진 모르겠지만.. 어쨌든 객체 타입의 key값이 K에 들어올 것이다..
+// 그리고 V는 그 값이 될 것.
+type Record<K extends keyof any, V> = {
+  [key in K]: V;
+};
+
+type Thumbnail = Record<"large" | "medium" | "small", { url: string }>;
