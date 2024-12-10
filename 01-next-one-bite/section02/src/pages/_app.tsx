bde430482669b7f9540116b7a36afc98989c6de1
Superduper-India <sunyoung1542@gmail.com>
2024-12-10 09:52:53 +0900

    Page Router 핵심 정리 - 네비게이팅

--- 01-next-one-bite/section02/src/pages/_app.tsx
+++ 01-next-one-bite/section02/src/pages/_app.tsx
@@ -1,11 +1,18 @@
 import type { AppProps } from "next/app";
+import Link from "next/link";
 
 // app컴포넌트는 루트 컴포넌트의 역할을 한다.
 // 그리고 페이지 역할을 하는 컴포넌트가 렌더링된다.
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <h2>글로벌 헤더</h2>
+      <header>
+        <Link href={"/"}>index</Link>
+        &nbsp;
+        <Link href={"/search"}>search</Link>
+        &nbsp;
+        <Link href={"/book/1"}>book/1</Link>
+      </header>
       <Component {...pageProps} />
     </>
   );
