9c224c2fb4f96851125073264f29617be5ff3474
Superduper-India <sunyoung1542@gmail.com>
2024-12-10 09:45:04 +0900

    Page Router 핵심 정리 - 페이지 라우팅 설정하기

---
+++ 01-next-one-bite/section02/src/pages/search/index.tsx
@@ -0,0 +1,12 @@
+import { useRouter } from "next/router";
+
+export default function Page() {
+  // 여기서 useRouter훅에 대해서 두 번 렌더링 되는건?....
+  // => 쿼리스트링의 값을 가져오기 위해서이다.
+  const router = useRouter();
+  console.log("dddd", router);
+  const { question } = router.query;
+  console.log("dddd 222", question);
+
+  return <h1>search</h1>;
+}
