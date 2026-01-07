2411e7866f027f2645167e4f26f1503259b8464b
Superduper-India <sunyoung1542@gmail.com>
2026-01-07 14:26:10 +0900

    chore: tailwind css 의존성 모듈 설치 및 프로젝트에 적용

--- 00-news-today/app/page.tsx
+++ 00-news-today/app/page.tsx
@@ -1,3 +1,21 @@
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+  return (
+    <div className="max-w-sm mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl m-5">
+      <div className="md:flex">
+        <div className="p-8">
+          <div className="uppercase tracking-wide text-sm text-indigo-500 font-semibold">
+            Tailwind 학습중
+          </div>
+          <a
+            href="#"
+            className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">
+            정말 배우기 쉬운 CSS 프레임워크
+          </a>
+          <p className="mt-2 text-slate-500">
+            클래스 몇 개만 추가했을 뿐인데 디자인이 완성되었어요!
+          </p>
+        </div>
+      </div>
+    </div>
+  );
 }
